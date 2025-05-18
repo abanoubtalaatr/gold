c082a4d71f1eca34dec5094bde5ba12cf3e59df6
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-05-18 20:20:39 +0300

    WIP

--- database/migrations/2024_03_22_000001_create_ratings_table.php
+++ database/migrations/2024_03_22_000001_create_ratings_table.php
@@ -8,6 +8,9 @@ return new class extends Migration
 {
     public function up(): void
     {
+        //drop the table if it exists
+        Schema::dropIfExists('ratings');
+
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
