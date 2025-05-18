0ee568c973b22083935f4ec4b4280f5507e8ec94
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2025-05-18 14:00:50 +0300

    WIP:

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
-window.axios = axios;
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '7125d79184ff70ea2808',
+    cluster: 'eu',
+    forceTLS: true
+});
