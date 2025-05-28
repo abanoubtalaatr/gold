5b1fb1a7d3e6ce6cbf85a1b484dd0a2f09506d15
ShahendaHemdan <shahenda@example.com>
2025-05-28 11:10:36 +0300

    convert api banner to slider

--- app/Http/Controllers/Api/V1/BannerController.php
+++ app/Http/Controllers/Api/V1/BannerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BannerResource;
 use App\Models\Banner;
+use App\Models\HomeSlider;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Traits\ApiResponseTrait;
@@ -14,15 +15,15 @@ class BannerController extends Controller
     use ApiResponseTrait;
     public function index(Request $request)
     {
-        $banners = Banner::query()
+        $home_sliders = HomeSlider::query()
             ->where('is_active', true)
             ->latest()
             ->get();
 
         return $this->successResponse(
-            BannerResource::collection($banners),
+            $home_sliders,
             __('mobile.banners_fetched_success')
         );
 
     }
-} 
+}
